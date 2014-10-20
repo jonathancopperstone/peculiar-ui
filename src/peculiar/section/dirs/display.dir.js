c6018d744befcd3021060ca8065ddc30677f39fa
Johnny Copperstone <jonnce@gmail.com>
2014-10-20 18:06:22 +0100

    Added label to display directive

--- src/peculiar/section/dirs/display.dir.js
+++ src/peculiar/section/dirs/display.dir.js
@@ -6,7 +6,15 @@
       restrict: 'E',
       replace: true,
       transclude: true,
-      templateUrl: 'src/peculiar/section/tpls/display.tpl.html'
+      templateUrl: 'src/peculiar/section/tpls/display.tpl.html',
+      link: function(scope, elem, attrs) {
+
+        var displayLabel = angular.isDefined(attrs.label) ? attrs.label : false;
+
+        if (displayLabel) {
+          scope.displayLabel = displayLabel;
+        }
+      }
     };
   });
 
